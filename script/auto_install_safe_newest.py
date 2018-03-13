d60be6d2c91f8c051ea9c7d97e106648020c33ee
Angus <angus9777@gmail.com>
2018-03-13 15:44:20 +0800

    fix bug: safe_newest

--- script/auto_install_safe_newest.py
+++ script/auto_install_safe_newest.py
@@ -24,9 +24,13 @@ def main():
     safe_stop()
 
     safe_home = '~/.safe'
-    #'http://anwang.com/download/safe_v1.0.1_linux.tar.gz'
+    #'http://anwang.com/download/safe_newest_linux.tar.gz'
     ret = 0
-    package_name = 'safe_v1.0.1_linux.tar.gz'
+    package_name = 'safe_newest_linux.tar.gz'
+    base_dir = 'safe_newest'
+    clean_file_cmd = 'rm -rf ' + base_dir + ' safe_v*'
+    ret = subprocess.call(clean_file_cmd, shell=True)
+
     url_base = 'http://www.anwang.com/download/'
     package_url = url_base + package_name
     ##step 1: download package
@@ -34,7 +38,7 @@ def main():
     ret = subprocess.call(get_package_cmd, shell=True)
     if ret == 0:
         ##step 2: parse package
-        parse_package_cmd = 'tar -xzvf ' + package_name
+        parse_package_cmd = 'tar -xzvf ' + package_name + ' && mv safe_v* ' + base_dir
         print parse_package_cmd
         ret = subprocess.call(parse_package_cmd, shell=True)
 
@@ -50,9 +54,9 @@ def main():
         cp_file_to_home = 'cp ./safe.conf ' + safe_home
         ret = subprocess.call(cp_file_to_home, shell=True)
 
-        cp_file_to_home = 'cp ./safe_v1.0.1/safed ' + safe_home
+        cp_file_to_home = 'cp ./' + base_dir + '/safed ' + safe_home
         ret = subprocess.call(cp_file_to_home, shell=True)
-        cp_file_to_home = 'cp ./safe_v1.0.1/safe-cli ' + safe_home
+        cp_file_to_home = 'cp ./' + base_dir + '/safe-cli ' + safe_home
         ret = subprocess.call(cp_file_to_home, shell=True)
 
         #step 5: change file's power
@@ -60,15 +64,16 @@ def main():
         ret = subprocess.call(change_file_power, shell=True)
 
         #step 6: copy depend libs to system dir
-        parse_dep = 'tar -xzvf ./safe_v1.0.1/deps/deps-ubuntu14.04.tar.gz -C ./safe_v1.0.1/deps/'
+        parse_dep = 'tar -xzvf ./' + base_dir + '/deps/deps-ubuntu14.04.tar.gz -C ./' + base_dir + '/deps/'
         ret = subprocess.call(parse_dep, shell=True)
         if ret == 0:
             #maybe need root password
-            cp_dep_to_gen_root = 'cp -rf ./safe_v1.0.1/deps/usr /'
+            cp_dep_to_gen_root = 'cp -rf ./' + base_dir + '/deps/usr /'
             print cp_dep_to_gen_root
             ret = subprocess.call(cp_dep_to_gen_root, shell=True)
             exec_safed = '~/.safe/safed &'
             ret = subprocess.call(exec_safed, shell=True)
+        print 'get package suc'
     else:
         print 'get package error'
 
